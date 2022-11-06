8076e4f7d741fb2ed7a111dd87dfe0f844359836
Isaac Milarsky <krabs@tilde.team>
2022-11-06 15:12:51 -0600

    Markers
    Signed-off-by: Isaac Milarsky <krabs@tilde.team>

--- src/hw4.cpp
+++ src/hw4.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -32,6 +33,28 @@ int main(int argc, char ** argv)
     cv::Mat kernel1 = cv::Mat::ones(3, 3, CV_8U);
     dilate(dist, dist, kernel1);
     imwrite("markers.png", dist);
+
+    //Create marker object for opencv watershed
+    cv::Mat dist_8u;
+    dist.convertTo(dist_8u, CV_8U);
+
+    //Find total markers
+    std::vector<std::vector< cv::Point> > contours;
+    cv::findContours(dist_8u,contours,cv::RETR_EXTERNAL,cv::CHAIN_APPROX_SIMPLE);
+
+    cv::Mat markers = cv::Mat::zeros(dist.size(),CV_32S);
+
+    //Forground markers
+    for (size_t iter = 0; iter < contours.size(); iter++)
+    {
+        cv::drawContours(markers,contours,static_cast<int>(iter), cv::Scalar(static_cast<int>(iter)+1), -1);
+    }
+
+    cv::circle(markers,cv::Point(5,5),3,cv::Scalar(255), -1);
+    cv::Mat markers8u;
+
+    markers.convertTo(markers8u, CV_8U,10);
+    imwrite("marker_processed.png",markers8u);
 
     return 0;
 }
